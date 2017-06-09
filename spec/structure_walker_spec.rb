e83d23176a8b56d779325dc0dd8672945320ece2
kadavar <kadavar32@gmail.com>
2017-06-09 17:51:43 +0300

    add specs

--- spec/structure_walker_spec.rb
+++ spec/structure_walker_spec.rb
@@ -1,11 +1,22 @@
-require "spec_helper"
+require 'spec_helper'
 
 RSpec.describe StructureWalker do
-  it "has a version number" do
+  it 'has a version number' do
     expect(StructureWalker::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe 'without errors' do
+
+    subject { StructureWalker::Builder.invoke(proc).call(steps, data) }
+
+    context 'with simple structure'
+    let(:data) { { key: [{ key: 'key' }, { key: 'key' }]} }
+    let(:result) { { key: [{ key: 'key', ok: 'data' }, { key: 'key', ok: 'data' }] } }
+    let(:proc) { ->(data) { data[:ok] = 'data'; data } }
+    let(:steps) { [[:enum, :hash], [:enum, :array]] }
+
+    it 'changes data with proc' do
+      expect(subject).to eq result
+    end
   end
 end
